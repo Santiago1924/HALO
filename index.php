49d1090e8e11887a5cf05f94044210ae9eac897a
Santiago1924 <duvilla24quevedo@gmail.com>
2025-09-22 15:57:12 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+
+    <div>
+        <h1>INICIAR SESION</h1>
+    </div>
+    
+    <form action="">
+
+    <div>    
+        <label for="usuario">USUARIO
+            <input type="text"> 
+        </label>
+    </div>
+        <div>    
+        <label for="password">CONTRASEÑA
+            <input type="password"> 
+        </label>
+    </div>
+
+    <div>
+        <a href="http://">Olvidaste tu contraseña?</a>
+    </div>
+        <div>
+        <a href="http://">registrarse</a>
+    </div>
+
+
+
+</form>
+
+
+    
+</body>
+</html>
